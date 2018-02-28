703dd5435a78e4106a2a7e589402e1cdcbf92bda
jinhu <hi.jinhu.zhang@gmail.com>
2018-02-28 18:37:32 +0800

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WechatBuilder::Application.config.secret_token = '8a62d1fcf4d17bef4c7f2a0a81c1023da6485aa4e272e22ab4ef04dc243e9f2cfc371726d579c055ae54adeadb467dd828a1b55b4d3e10c023086395b35422b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WechatBuilder::Application.config.session_store :cookie_store, key: '_wechat-builder_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WechatBuilder::Application.config.session_store :active_record_store
